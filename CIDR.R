15be1ff26ebb229ec18c0a92a1cd2298a0b58e3c
Nick L'Heureux <nlheureux12@gmail.com>
2024-04-29 05:05:45 -0400

    changes.

--- CIDR.R
+++ CIDR.R
@@ -6,26 +6,60 @@ if (!require(package_name, character.only = TRUE)) {
   # If not installed, install the package
   devtools::install_github("VCCRI/CIDR")
 }
+# Install the dplyr package if not already installed
+install.packages("dplyr")
+
+# Load the dplyr package
+library(dplyr)
 
 library(cidr)
 
 ## Read in data
 humanTags <- read.delim("counts.human.tsv")
+
+# Get the number ofhdata)
+num_rows <- nrow(humanTags)
+
+# Calculate the midpoint
+midpoint <- floor(num_rows / 2)
+
+# Split the data into two halves
+first_half <- humanTags[1:midpoint, ]
+second_half <- humanTags[(midpoint + 1):num_rows, ]
+# Shuffle the data
+shuffled_data <- humanTags[sample(nrow(humanTags)), ]
+
+# Split the shuffled data into two halves
+first_half <- shuffled_data[1:midpoint, ]
+second_half <- shuffled_data[(midpoint + 1):num_rows, ]
+
+
+filtered_data <- humanTags %>% select(-starts_with("M"))
+
+# Assuming 'data' is your original data frame and 'sd_noise' is the standard deviation of the Gaussian noise
+# 'mean_noise' is the mean, usually 0 for Gaussian noise
+
+# Generate Gaussian noise
+noise <- rnorm(nrow(humanTags))
+
+# Add noise to your data
+noisy_data <- humanTags + noise
+
 # -------------------------------------------------
 ## Read in annotation
-info <- colnames(humanTags)
+info <- colnames(filtered_data)
 # Filter on the celltype prefixs
 # Match Fibroblasts
 # Match Fibroblasts
-fibroblasts_matches <- grep("fibroblasts", tolower(colnames(humanTags)), value = TRUE)
+fibroblasts_matches <- grep("fibroblasts", tolower(colnames(filtered_data)), value = TRUE)
 fibroblasts_matches_clean <- sub("_.*", "", fibroblasts_matches)
 
 # Match HEK293T
-hek293t_matches <- grep("hek293t", tolower(colnames(humanTags)), value = TRUE)
+hek293t_matches <- grep("hek293t", tolower(colnames(filtered_data)), value = TRUE)
 hek293t_matches_clean <- sub("_.*", "", hek293t_matches)
 
 # Match MCF7
-mcf7_matches <- grep("mcf7", tolower(colnames(humanTags)), value = TRUE)
+mcf7_matches <- grep("mcf7", tolower(colnames(filtered_data)), value = TRUE)
 mcf7_matches_clean <- sub("_.*", "", mcf7_matches)
 
 # Combine all matches into a single vector
@@ -43,19 +77,19 @@ for (i in 1:length(cols)){
 }
 
 ##  Standard principal component analysis using prcomp
-priorTPM <- 1
-humanTags10 <- humanTags[rowSums(humanTags)>10,]
-humanTags10_lcpm <- log2(t(t(humanTags10)/colSums(humanTags10))*1000000+priorTPM)
-pca <- prcomp(t(humanTags10_lcpm))
-plot.new()
-legend("center", legend=types, col=scols, pch=1)
-plot(pca$x[,c(1,2)], col=cols, pch=1,
-     xlab="PC1", ylab="PC2",
-     main="Principal Component Analysis (prcomp)")
+#priorTPM <- 1
+#humanTags10 <- humanTags[rowSums(humanTwoTags)>10,]
+#humanTags10_lcpm <- log2(t(t(humanTags10)/colSums(humanTags10))*1000000+priorTPM)
+#pca <- prcomp(t(humanTags10_lcpm))
+#plot.new()
+#legend("center", legend=types, col=scols, pch=1)
+#plot(pca$x[,c(1,2)], col=cols, pch=1,
+#     xlab="PC1", ylab="PC2",
+#     main="Principal Component Analysis (prcomp)")
 
 #### CIDR ######
 ################
-scHuman <- scDataConstructor(as.matrix(humanTags))
+scHuman <- scDataConstructor(as.matrix(filtered_data))
 scHuman <- determineDropoutCandidates(scHuman)
 scHuman <- wThreshold(scHuman)
 scHuman <- scDissim(scHuman)
@@ -71,11 +105,14 @@ plot.new()
 legend("center", legend=types, col=scols, pch=15)
 plot(scHuman@PC[,c(1,2)], col=cols, pch=scHuman@clusters,
      main="CIDR", xlab="PC1", ylab="PC2")
-scHuman@dscThreshold
+scHuman@dThreshold
 ## Use Adjusted Rand Index to measure the accuracy of CIDR clustering
+scHuman@clusters
 ARI_CIDR <- adjustedRandIndex(scHuman@clusters,cols)
 ARI_CIDR
 ## 0.8977449
+sd(scHuman@clusters)
+scHuman@nCluster
 
 ## This section shows how to alter the parameters of CIDR
 scHuman@nPC
@@ -89,6 +126,7 @@ plot(scHuman@PC[,c(1,2)],
 ARI_CIDR <- adjustedRandIndex(scHuman@clusters,cols)
 ARI_CIDR
 ## 0.8977449
+sd(scHuman@clusters)
 
 ## This section shows how to alter the parameters of CIDR
 scHuman@nPC
@@ -113,6 +151,7 @@ plot(scHuman@PC[,c(1,2)], col=cols, pch=scHuman@clusters,
 ARI_CIDR <- adjustedRandIndex(scHuman@clusters,cols)
 ARI_CIDR
 ## 0.7294235
+sd(scHuman@clusters)
 
 ## Use a different imputation weighting threshold
 ## Default 6.55
@@ -128,3 +167,5 @@ plot(scHuman@PC[,c(1,2)], col=cols, pch=scHuman@clusters,
 ARI_CIDR <- adjustedRandIndex(scHuman@clusters,cols)
 ARI_CIDR
 ## 0.8101352
+sd(scHuman@clusters)
+
--- sc3.R
+++ sc3.R
@@ -2,11 +2,36 @@
 
 #BiocManager::install("SC3")
 #BiocManager::install("scater")
+
+
+###Prep count matrix and metadata
+plate<-gsub('^(.*?_.*?_.*?)_.*', "\\1",  colnames(counts))
+
+plate.md<-cbind(colnames(counts),plate)
+plate.md<-data.frame(plate.md)
+rownames(plate.md)<-unique(plate.md$V1)
+
+celltype<-ifelse(grepl("d0",plate), "d0",
+                 ifelse(grepl("d4",plate), 'd4', 
+                        ifelse(grepl("d8",plate), 'd8',
+                               'd12')))
+
+celltype.md<-cbind(colnames(counts),celltype)
+celltype.md<-data.frame(celltype.md)
+rownames(celltype.md)<-celltype.md$V1
+counts<-as.matrix(counts)
+
+
+
 browseVignettes("SC3")
 
 library(SingleCellExperiment)
 library(SC3)
 library(scater)
+install.packages("mclust")
+library(mclust)
+library(dplyr)
+
 
 d0_matches <- grep("d0", colnames(mouseTags), value = TRUE)
 d0_matches_clean <- sub(".*d0.*", "d0", d0_matches)
@@ -65,11 +90,44 @@ plotPCA(
 )
 
 #plotPCA(sce, colour_by = "cell_type1")
-sce <- sc3(sce, ks = 2:4, biology = TRUE)
-sc3_plot_consensus(sce, k = 3)
+sce <- sc3(sce, ks = 2:5, biology = TRUE)
+sc3_plot_consensus(sce, k = 5)
+sc3_plot_markers(sce, k = 3)
+
+
 sc3_export_results_xls(sce)
 
 ########################################################################
+humanTags <- read.delim("counts.human.tsv")
+
+# Get the number ofhdata)
+num_rows <- nrow(humanTags)
+
+# Calculate the midpoint
+midpoint <- floor(num_rows / 2)
+
+# Split the data into two halves
+first_half <- humanTags[1:midpoint, ]
+second_half <- humanTags[(midpoint + 1):num_rows, ]
+# Shuffle the data
+shuffled_data <- humanTags[sample(nrow(humanTags)), ]
+
+# Split the shuffled data into two halves
+first_half <- shuffled_data[1:midpoint, ]
+second_half <- shuffled_data[(midpoint + 1):num_rows, ]
+
+filtered_data <- humanTags %>%
+  select(-starts_with("M"))
+
+# Assuming 'data' is your original data frame and 'sd_noise' is the standard deviation of the Gaussian noise
+# 'mean_noise' is the mean, usually 0 for Gaussian noise
+
+# Generate Gaussian noise
+noise <- rnorm(nrow(humanTags))
+
+# Add noise to your data
+noisy_data <- humanTags + noise
+
 info <- colnames(humanTags)
 # Filter on the celltype prefixs
 # Match Fibroblasts
@@ -99,8 +157,6 @@ for (i in 1:length(cols)){
   cols[i] <- scols[which(types==cellType[i])]
 }
 
-col_data <- colData(sce)
-
 # create a SingleCellExperiment object
 sce <- SingleCellExperiment(
   assays = list(
@@ -116,19 +172,63 @@ rowData(sce)$feature_symbol <- rownames(sce)
 sce <- sce[!duplicated(rowData(sce)$feature_symbol), ]
 sce <- runPCA(sce)
 
-plotPCA(
-  sce,
-  colour_by = "X"
+# Think about changing parameters for each step2
+sce <- sc3(sce, ks = 2:4, biology = TRUE)
+adjustedRandIndex(colData(sce)$sc3_3_clusters, cols) # 1
+
+twotypessce <- sce
+no_svm_labels_twotypes <- colData(twotypessce)$sc3_3_clusters # 1
+adjustedRandIndex(no_svm_labels_twotypes, cols) # [1] 0.996414
+sc3_plot_consensus(
+  twotypessce, k = 3, 
+  show_pdata = c(
+    "X", 
+    "sc3_3_clusters", 
+    "sc3_3_log2_outlier_score"
+  )
 )
 
-# Think about changing parameters for each step
-sce <- sc3(sce, ks = 2:4, biology = TRUE)
+first_half_sce = sce
+sc3_plot_consensus(
+  twotypessce, k = 3, 
+  show_pdata = c(
+    "X", 
+    "sc3_3_clusters", 
+    "sc3_3_log2_outlier_score"
+  )
+)
+no_svm_labels_firsthalf <- colData(first_half_sce)$sc3_3_clusters # 1
+adjustedRandIndex(no_svm_labels_firsthalf, cols)
+
+
+new_sce = sce
+no_svm_labels <- colData(sce)$sc3_3_clusters
+adjustedRandIndex(no_svm_labels, cols) # 1
+# Assuming 'sce' is your SingleCellExperiment object
+
+# Step 1: Extract the count data from the SingleCellExperiment object
+count_data <- assays(sce)$sc3_3_clusters
+sd(count_data)
+
+sc3_plot_markers(sce, k = 3)
+
+sc3_plot_consensus(
+  sce, k = 3, 
+  show_pdata = c(
+    "X", 
+    "sc3_3_clusters", 
+    "sc3_3_log2_outlier_score"
+  )
+)
+
 sc3_plot_de_genes(sce, k = 3) # needs to be 3 for human
 sc3_plot_consensus(sce, k = 3)
 sc3_export_results_xls(sce)
 
+k <- sc3_estimate_k(sce)
+sc3_estimate_k(sce)
+str(metadata(sce)$sc3)
 sc3_plot_cluster_stability(sce, k = 3)
-
 plotPCA(
   sce,
   colour_by = "X"
